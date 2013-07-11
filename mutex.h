22cd0c688a9dc1fa20008732cbbe36440ff0eecc
Tamara Meneghetti da Cruz <tamara.meneghetti@gmail.com>
2013-07-10 22:16:33 -0300

    Mutex.h
    Mutex.h final final

--- mutex.h
+++ mutex.h
@@ -1,4 +1,4 @@
-#ifndef MUTEX_H
+ #ifndef MUTEX_H
 #define	MUTEX_H
 
 #ifdef	__cplusplus
@@ -9,11 +9,8 @@ extern "C" {
 #include <stdlib.h>
 #include <pthread.h>
 
-void lock(pthread_mutex_t the_mutex, pthread_cond_t cond);
-void unlock(pthread_mutex_t the_mutex);
-void wait(pthread_cond_t cond);
-void wakeup(pthread_cond_t cond);
-
+void lock(int mutex);
+void unlock(int mutex);
 #ifdef	__cplusplus
 }
 #endif
