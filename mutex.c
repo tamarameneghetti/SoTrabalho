609bce09465d2a4e28695457acf42ffe9bb3a618
Tamara Meneghetti da Cruz <tamara.meneghetti@gmail.com>
2013-07-10 22:14:32 -0300

    Mutex.c
    mutex final

--- mutex.c
+++ mutex.c
@@ -1,17 +1,13 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 #include "mutex.h"
 
-void lock(pthread_mutex_t the_mutex, pthread_cond_t cond){
-	pthread_mutex_lock(&the_mutex);
+void lock(int mutex){
+	__sync_fetch_and_add(&mutex, 1);
 }
 
-void unlock(pthread_mutex_t the_mutex){
-	pthread_mutex_unlock(&the_mutex);
-}
-
-void wait(pthread_cond_t cond){
-	pthread_cond_wait(&cond, &the_mutex);
-}
-
-void wakeup(pthread_cond_t cond){
-	pthread_cond_signal(&cond);
+void unlock(int mutex){
+	__sync_fetch_and_sub(&mutex, 1);
 }
